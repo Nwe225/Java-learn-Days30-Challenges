57c68c575ee69e9ab546be42b6fd026282a46088
Win Nwe Soe <winnwes89@gmail.com>
2026-07-01 02:37:13 -0700

    CompoundCalculator

---
+++ src/CompoundCalculator.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+ public class CompoundCalculator {
+     public static void main(String[] args){
+
+         Scanner scanner=new Scanner(System.in);
+
+         double principal;
+         double rate;
+         int timesCompounded;
+         int years;
+         double amount;
+
+         System.out.print("Enter the principal amount: ");
+         principal=scanner.nextDouble();
+
+         System.out.print("Enter the interest rate(in %): ");
+         rate=scanner.nextDouble();
+
+         System.out.print("Enter the # of times interest is compounded per year: ");
+         timesCompounded=scanner.nextInt();
+
+         System.out.print("Enter the # of years: ");
+         years=scanner.nextInt();
+
+         amount=principal*Math.pow(1+rate/timesCompounded,timesCompounded*years);
+
+         System.out.println("The amount after "+years+" years is: "+String.format("%.2f",amount));
+         scanner.close();
+     }
+
+}
