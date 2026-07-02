93a9fdc8f4f0441a0bbcf133e22e4cc5c43bacd0
Win Nwe Soe <winnwes89@gmail.com>
2026-07-02 02:43:33 -0700

    Printf.java

---
+++ src/Printf.java
@@ -0,0 +1,16 @@
+public class Printf{
+    public static void main(String[] args){(
+        String name="Win";
+        char firstLetter='W';
+        int age=30;
+        double height=60.5;
+        boolean isStudent=true;
+
+        System.out.printf("Hello %s\n",name);
+        System.out.printf("Your name starts with a %c\n",firstLetter);
+        System.out.printf("You are %d years old\n",age);
+        System.out.printf("Your height is %.1f inches\n",height);
+        System.out.printf("Are you a student? %b\n",isStudent);
+
+    }
+}
