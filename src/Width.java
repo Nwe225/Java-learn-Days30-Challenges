7ccdf54e166e987b0d0a3f7aba2d8e4966feed98
Win Nwe Soe <winnwes89@gmail.com>
2026-07-03 06:52:11 -0700

    Width

---
+++ src/Width.java
@@ -0,0 +1,16 @@
+public class Width{
+    public static void main(String[] args){
+
+        int id1=1;
+        int id2=23;
+        int id3=455;
+        int id4=7809;
+
+        System.out.printf("%4d\n",id1);
+        System.out.printf("%4d\n",id2);
+        System.out.printf("%4d\n",id3);
+        System.out.printf("%4d\n",id4);
+
+    }
+
+}
