705e3c155c518d55492641b119741885c1a7069f
Win Nwe Soe <winnwes89@gmail.com>
2026-07-04 02:40:07 -0700

    Maths

--- src/MathsClass.java
+++ src/MathsClass.java
@@ -8,25 +8,25 @@ public class MathsClass {
         double result6;
         double result7;
 
-        result1=Maths.pow(2,5);
+        result1=Math.pow(2,5);
         System.out.println(result1);
 
-        result2=Maths.sqrt(9);
+        result2=Math.sqrt(9);
         System.out.println(result2);
 
-        result3=Maths.abs(-5);
+        result3=Math.abs(-5);
         System.out.println(result3);
 
-        result4=Maths.round(3.14);
+        result4=Math.round(3.14);
         System.out.println(result4);
 
-        result5=Maths.floor(3.99);
+        result5=Math.floor(3.99);
         System.out.println(result5);
 
-        result6=Maths.max(10,20);
+        result6=Math.max(10,20);
         System.out.println(result6);
 
-        result7=Maths.min(10,20);
+        result7=Math.min(10,20);
         System.out.println(result7);
 
 
