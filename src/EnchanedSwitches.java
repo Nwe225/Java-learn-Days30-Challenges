dbf71f7794c14de8d0b6e3d7db6a67f194813f78
Win Nwe Soe <winnwes89@gmail.com>
2026-07-29 02:13:11 -0700

    EnchanedSwitches

---
+++ src/EnchanedSwitches.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class EnchanedSwitches {
+    public static void main(String[] args){
+        Scanner scanner=new Scanner(System.in);
+
+        System.out.print("Enter the day of the week: ");
+        String day=scanner.nextLine();
+        switch (day) {
+            case "Monday", "Tuesday", "Wednesday", "Thursday", "Friday" -> System.out.println("It's a weekday.");
+            case "Saturday", "Sunday" -> System.out.println("It's a weekend.");
+            default -> System.out.println("Invalid day of the week.");
+        }
+
+        scanner.close();
+        }
+    }
